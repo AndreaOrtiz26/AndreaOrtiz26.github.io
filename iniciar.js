b9e8afd4698d9846890634fe95e6c13b9b137501
AndreaOrtiz26 <106268075+AndreaOrtiz26@users.noreply.github.com>
2023-02-06 21:45:47 -0600

    Add files via upload

---
+++ iniciar.js
@@ -0,0 +1,11 @@
+function ir(){
+
+var c=9876;
+var u="Andrea";
+if(document.form.password.value==c && document.form.login.value==u){
+alert ("Bienvenido");}
+else{
+alert("Los datos ingresados son incorrectos, intenta de nuevo.");
+}
+
+} 
